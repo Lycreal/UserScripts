c61346ec26afdf6589805d93922b97a325ec6d6c
Lycreal <jgsly123@gmail.com>
2022-06-29 21:24:52 +0800

    Add CCTransparency

---
+++ CCTransparency/CCTransparency.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         cc直播弹幕透明
+// @namespace    https://github.com/Lycreal/UserScripts
+// @version      0.1
+// @description  将网易cc直播间的弹幕透明度设置为50%
+// @author       Lycreal
+// @match        *://cc.163.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let codeBlockStyle = document.createElement('style');
+    codeBlockStyle.innerText = `
+      .cc-h5player-container .comment-canvas{
+        opacity: 0.5;
+      }`;
+    document.body.appendChild(codeBlockStyle);
+})();
